aacf68c2d655aeb9f1c623af5cb995e202a97259
Vuurvos1 <Vuurvos1@users.noreply.github.com>
2020-06-10 18:11:38 +0200

    rendering data and template fixes

--- routes/routes.js
+++ routes/routes.js
@@ -4,11 +4,15 @@ const router = express.Router();
 // Routes
 const index = require('./index');
 const users = require('./users');
+const login = require('./login');
+const register = require('./register');
 const liken = require('./likepage');
 const error = require('./error');
 
 router.use('/', index);
 router.use('/', users);
+router.use('/', login);
+router.use('/', register);
 router.use('/', liken);
 router.use('/', error);
 
