b783d8ab64fcc4334feddc0d5a6ecfac71543d3d
Simon Planje <simon.planje2000@gmail.com>
2020-06-10 12:51:59 +0200

    statisch ejs pagina likepae

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
 // const mongodb = require('mongodb');
 // const ObjectId = mongo.ObjectID;
 // const session = require('express-session');
 // const cookieParser = require('cookie-parser');
 
-const http = require('http');
+const http = require("http");
 const port = process.env.PORT || 3000;
 let app = express();
 let server = http.createServer(app);
@@ -14,20 +14,20 @@ let server = http.createServer(app);
 let db;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}`;
 
-app.use(express.static(__dirname + '/views'));
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.use(express.static(__dirname + "/views"));
+app.set("view engine", "ejs");
+app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // app.use(cookieParser());
 // app.use(session);
 
 // Routes
-const index = require('./routes/index');
-const error = require('./routes/error');
+const index = require("./routes/index");
+const error = require("./routes/error");
 
-app.use('/', index);
-app.use('/', error);
+app.use("/", index);
+app.use("/", error);
 
 app.listen(port, () =>
   console.log(`App listening at http://localhost:${port}`)
