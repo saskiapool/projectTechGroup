63e38ab6428624235e07fbdd67b67ce8cd50fc5f
Simon Planje <simon.planje2000@gmail.com>
2020-06-11 13:57:24 +0200

    header and a piece of the like page

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const db = require('./helper/db');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const db = require("./helper/db");
 // const session = require('express-session');
 // const cookieParser = require('cookie-parser');
 
-const http = require('http');
+const http = require("http");
 const port = process.env.PORT || 3000;
 let app = express();
 let server = http.createServer(app);
 
-app.use(express.static(__dirname + '/views'));
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.use(express.static(__dirname + "/views"));
+app.set("view engine", "ejs");
+app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // app.use(cookieParser());
 // app.use(session);
 
-app.use(require('./routes/routes'));
+app.use(require("./routes/routes"));
 
 db.connect(() => {
   app.listen(port, () => {
