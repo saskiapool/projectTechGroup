2cad438624ad8ff14091fdba3ae263e404feca6c
Simon Planje <simon.planje2000@gmail.com>
2020-06-13 15:29:52 +0200

    niet werkende liken page :(

--- routes/likepage.js
+++ routes/likepage.js
@@ -1,8 +1,38 @@
 const express = require('express');
+const db = require('../helper/db');
 const router = express.Router();
 
 router.get('/like', (req, res) => {
-  req.session.user ? res.render('./liken.ejs') : res.render('./login.ejs');
+  if (req.session.user) {
+    db.get()
+      .collection('users')
+      .find({})
+      .toArray()
+      .then((data) => {
+        res.render('./liken.ejs', { data: data });
+      });
+    //     (err, result) => {
+    //       if (err) {
+    //         console.log(err);
+    //       }
+    //       if (result) {
+    //         console.log(result);
+    //         res.render('./liken.ejs', { data: result });
+    //       }
+    //     });
+    // }
+  } else {
+    res.render('./login.ejs');
+  }
 });
+
+// db.get()
+// .collection('users')
+// .find({})
+// .toArray()
+// .then((users) => {
+//   console.log('users', users);
+//   res.render('chat.ejs', { data: users });
+// });
 
 module.exports = router;
