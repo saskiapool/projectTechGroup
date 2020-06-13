a41ce3ae837866bdb8f1b72e8b28b8a293414d95
Vuurvos1 <Vuurvos1@users.noreply.github.com>
2020-06-13 15:48:12 +0200

    ejs fix

--- routes/likepage.js
+++ routes/likepage.js
@@ -6,33 +6,13 @@ router.get('/like', (req, res) => {
   if (req.session.user) {
     db.get()
       .collection('users')
-      .find({})
-      .toArray()
-      .then((data) => {
+      .find()
+      .toArray((err, data) => {
         res.render('./liken.ejs', { data: data });
       });
-    //     (err, result) => {
-    //       if (err) {
-    //         console.log(err);
-    //       }
-    //       if (result) {
-    //         console.log(result);
-    //         res.render('./liken.ejs', { data: result });
-    //       }
-    //     });
-    // }
   } else {
     res.render('./login.ejs');
   }
 });
-
-// db.get()
-// .collection('users')
-// .find({})
-// .toArray()
-// .then((users) => {
-//   console.log('users', users);
-//   res.render('chat.ejs', { data: users });
-// });
 
 module.exports = router;
