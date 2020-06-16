cc6ab559acbcad8f7148078efa73edcbe2c6ae3d
Saskia <poolsaskia@gmail.com>
2020-06-16 16:59:55 +0200

    error in questionjs

--- routes/questionOne.js
+++ routes/questionOne.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../helper/db');
 
 router.get('/questionOne', (req, res) => {
   // res.render('./index.ejs');
-  // req.session.user ? res.render('./questionone.ejs') : res.render('./login.ejs');
-  res.render('./questionOne.ejs')
+req.session.user ? res.render('./questionone.ejs') : res.render('./login.ejs');
+  // res.render('./questionOne.ejs');
 });
 
-module.exports = router;
+router.post('/questionOne', async (req, res) => {
+  const answer = req.body.answer;
+
+  const data = {
+    yes: req.body.answer,
+    no: req.body.answer,
+  };
+
+  db.get().collection('questions').insertOne(data);
+  console.log(`added ${data} to the database`);
+  res.redirect('/chat');
+
+
+  module.exports = router;
+});
