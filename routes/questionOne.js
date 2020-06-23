58518914a3b9347fc25a34f8514ffb81f7301cce
Saskia <poolsaskia@gmail.com>
2020-06-23 09:23:18 +0200

    progressive enhancement questions

--- routes/questionOne.js
+++ routes/questionOne.js
@@ -1,17 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../helper/db');
-const session = require('express-session')
-
-const app = express()
-app.use(session({
-  'secret': '343ji43j4n3jn4jk3n'
-}))
 
 router.get('/questionOne', (req, res) => {
   req.session.user
    ? res.render('./questionOne.ejs')
-   : res.render('./questionOne.ejs');
+   : res.render('./login.ejs');
   // res.render('./questionOne.ejs');
 });
 
@@ -21,8 +15,6 @@ router.post('/questionOne', async (req, res) => {
   };
 
   db.get().collection('questions').insertOne(data);
-  console.log(`added ${data} to the database`);
-
   res.redirect('/questionTwo');
 });
 
--- routes/questionTwo.js
+++ routes/questionTwo.js
@@ -1,17 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../helper/db');
-const session = require('express-session')
-
-const app = express()
-app.use(session({
-  'secret': '343ji43j4n3jn4jk3n'
-}))
 
 router.get('/questionTwo', (req, res) => {
   req.session.user 
    ? res.render('./questionTwo.ejs') 
-   : res.render('./questionTwo.ejs');
+   : res.render('./login.ejs');
 // res.render('./questionOne.ejs');
 });
 
@@ -23,7 +17,6 @@ router.post('/questionTwo', async (req, res) => {
   console.log(`post question one data: ${data}`);
 
   db.get().collection('questions').insertOne(data);
-  console.log(`added ${data} to the database`);
   res.redirect('/questionThree');
 });
 
